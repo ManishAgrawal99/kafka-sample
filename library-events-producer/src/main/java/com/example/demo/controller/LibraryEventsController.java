52598c47d24b609e27713fc467f6d06f39409f92
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-07-14 23:46:23 +0530

    Using Controller to send events to Kafka Producer

--- library-events-producer/src/main/java/com/example/demo/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/example/demo/controller/LibraryEventsController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,15 +8,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.LibraryEvent;
+import com.example.demo.producer.LibraryEventProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 @RestController
 public class LibraryEventsController {
 	
+	@Autowired
+	LibraryEventProducer libraryEventProducer;
+	
 	@PostMapping("/v1/libraryEvent")
-	public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
+	public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException{
 		
 		//invoke kafka producer
 		System.out.println(libraryEvent);
+		
+		libraryEventProducer.sendLibraryEvent(libraryEvent);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
 	}
